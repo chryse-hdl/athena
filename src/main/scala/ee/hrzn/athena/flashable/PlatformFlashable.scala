aa80c821999c931052fbeea71e16d3ec448b4b39
Asherah Connor <ashe@kivikakk.ee>
2024-06-07 17:15:29 +0300

    adjust for zigsim.

--- src/main/scala/ee/hrzn/athena/flashable/PlatformFlashable.scala
+++ src/main/scala/ee/hrzn/athena/flashable/PlatformFlashable.scala
@@ -1,16 +1,29 @@
+/* Copyright © 2024 Asherah Connor.
+ *
+ * This file is part of Athena.
+ *
+ * Athena is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Athena is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Athena. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ee.hrzn.athena.flashable
 
-import ee.hrzn.chryse.tasks.BaseTask
+import ee.hrzn.chryse.build.CommandRunner._
 
 trait PlatformFlashable {
   var romFlashBase: BigInt
   def romFlashCommand(binPath: String): Seq[String]
 
   def programRom(binPath: String): Unit =
-    programRomImpl(binPath)
-
-  object programRomImpl extends BaseTask {
-    def apply(binPath: String): Unit =
-      runCmd(CmdStepProgram, romFlashCommand(binPath))
-  }
+    runCmd(CmdStep.Program, romFlashCommand(binPath))
 }
--- src/main/scala/ee/hrzn/athena/flashable/SubcommandRom.scala
+++ src/main/scala/ee/hrzn/athena/flashable/SubcommandRom.scala
@@ -1,17 +1,34 @@
+/* Copyright © 2024 Asherah Connor.
+ *
+ * This file is part of Athena.
+ *
+ * Athena is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Athena is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Athena. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ee.hrzn.athena.flashable
 
 import ee.hrzn.chryse.ChryseApp
 import ee.hrzn.chryse.ChryseSubcommand
+import ee.hrzn.chryse.build.writePath
 import ee.hrzn.chryse.platform.cxxrtl.CxxrtlPlatform
-import ee.hrzn.chryse.tasks.BaseTask
 
 import java.io.FileOutputStream
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
 
 abstract class SubcommandRom(chryse: ChryseApp)
-    extends ChryseSubcommand("rom")
-    with BaseTask {
+    extends ChryseSubcommand("rom") {
   banner("Build the ROM image, and optionally program it.")
   val program =
     if (chryse.targetPlatforms.length > 1)
@@ -29,7 +46,9 @@ abstract class SubcommandRom(chryse: ChryseApp)
   def romContent: Array[Byte]
 
   private def generate(): String = {
-    Files.createDirectories(Paths.get(buildDir))
+    val buildDir = "build"
+
+    Files.createDirectories(Path.of(buildDir))
 
     val content = romContent
     val binPath = s"$buildDir/rom.bin"
@@ -44,7 +63,7 @@ abstract class SubcommandRom(chryse: ChryseApp)
   def generateCxxrtl(platform: CxxrtlPlatform): Unit = {
     val romFlashBase = platform.asInstanceOf[PlatformFlashable].romFlashBase
     val content      = romContent
-    writePath(s"$buildDir/rom.cc") { wr =>
+    writePath(s"${platform.buildDir}/rom.cc") { wr =>
       wr.print("const uint8_t spi_flash_content[] = \"");
       wr.print(content.map(b => f"\\$b%03o").mkString)
       wr.println("\";");
